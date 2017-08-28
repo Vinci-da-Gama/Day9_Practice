c9b03ecaf50ea8a1cd50779181e7240b2b1e8e88
Vinci-da-Gama <11582145@student.uts.edu.au>
2017-08-29 09:24:07 +1000

    10

--- Day9/L9_L11/L11LinqRetrunObj.cs
+++ Day9/L9_L11/L11LinqRetrunObj.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 using Day9.L1ObjCollection;
 
@@ -25,6 +26,19 @@ namespace Day9.L9_L11
             {
                 Console.WriteLine("30 -- Order-Date : {0}.", o.ToShortDateString());
             }
+            // select one sepcific column
+            var od0 = from o in Orders
+                      select o.OrderDate;
+
+            // select multiple columns and return as annonymous type
+            var od1 = from o in Orders
+                      select new { o.OrderDate, o.OrderId };
+
+            // select multiple columns and return as real new type
+            var od2 = from o in Orders
+                      select new Order_Summary { od = o.OrderDate, oi = o.OrderId };
+
+            // 
         }
     }
 }
---
+++ Day9/L9_L11/Order_Summary.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day9.L9_L11
+{
+    class Order_Summary
+    {
+        public DateTime od { get; set; }
+        public int oi { get; set; }
+    }
+}
